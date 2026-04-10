70a3437058ebf7eb05238bb09df3fbce16eb1689
Baptiste geffroy <geffroy.baptiste.dev@gmail.com>
2026-04-11 00:05:18 +0200

    feat(async): implement fetchUserById by composing fetchUsers and find

--- exercises/03-async/index.ts
+++ exercises/03-async/index.ts
@@ -1,44 +1,54 @@
 import { users, User } from './users';
 
+// rajout entrainement pour simuler un délai d'attente
+const delay = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
+const countdown = async () => {
+  for (let i = 3; i > 0; i--) {
+    console.log(`${i}`);
+    await delay(1000);
+  }
+};
 // Exo 1 - Simuler une récupération asynchrone (API)
 // Objectif:
 // Simuler un appel API qui retourne la liste des users après un délai.
 
 const fetchUsers = async (): Promise<User[]> => {
-  return new Promise((resolve) => {
-    console.log('pending');
-    setTimeout(() => {
-      console.log('1sec');
-    }, 1000);
-    setTimeout(() => {
-      console.log('2sec');
-    }, 2000);
-    setTimeout(() => {
-      console.log('3ec');
-    }, 3000);
-    setTimeout(() => {
-      resolve(users);
-    }, 4000);
-  });
+  console.log('pending');
+  await countdown();
+  return users;
+  // return new Promise((resolve) => {
+  //   setTimeout(() => {
+  //     resolve(users);
+  //   }, 3000);
+  // });
 };
 
-const run = async (): Promise<void> => {
+const runFetchUsers = async (): Promise<void> => {
   const result = await fetchUsers();
+  console.log('Exo 1:');
   console.log(result);
 };
 
-run();
+// runFetchUsers();
 
 // Exo 2 — Récupérer un user par id de façon asynchrone
 // Objectif:
-// attendre la récupération des users
+// attendre la récupération des users (fetchUsers)
 // retourner le user correspondant à l’id
 
-// export const fetchUserById = async (
-//   id: number
-// ): Promise<User | undefined> => {
-//   // ...
-// };
+export const fetchUserById = async (id: number): Promise<User | undefined> => {
+  const users = await fetchUsers();
+  return users.find((user) => user.id === id);
+};
+
+const runFetchUserById = async (): Promise<void> => {
+  const result = await fetchUserById(2);
+  console.log('Exo 2:');
+  console.log(result);
+};
+runFetchUserById();
 
 // Exo 3 — Gérer le cas d’erreur
 // Objectif:
